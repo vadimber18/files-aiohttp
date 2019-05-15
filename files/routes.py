856b9861001ec2e505a16a122df381090cc74603
Vadim Bereznitsky <vadimber1994@mail.ru>
2019-05-15 18:49:54 -0400

    removed setup static routes for local

--- files/routes.py
+++ files/routes.py
@@ -20,8 +20,8 @@ def setup_routes(app):
     app.router.add_route('POST', '/api/files/upload', file_upload)
     app.router.add_route('GET', '/api/files/download/{file_id}', file_download)
 
-    if app['config']['debug']:
-        setup_static_routes(app)
+    #if app['config']['debug']:
+    #    setup_static_routes(app)
 
 
 def setup_cors(app):
